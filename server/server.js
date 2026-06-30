d399a1b067b4eec33af859f0aa4028d4ad1fc802
Zaara-Fatima <bano6096@gmail.com>
2026-06-30 16:59:05 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -6,9 +6,10 @@ import dotenv from "dotenv"
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js"
 
 const app = express()
+    dotenv.config()
 app.use(express.json())
 app.use(cors({
-    origin:["https://task-tracker-kss6.vercel.app/", "http://localhost:5173/"]
+    origin:["https://task-tracker-kss6.vercel.app", "http://localhost:5173"]
 ))
 app.use(express.urlencoded({ extended: true }));
 
@@ -16,8 +17,11 @@ app.use("/api/tasks", tasksRouter);
 app.use(notFound);
 app.use(errorHandler);
 
-dotenv.config()
+
 connectDB()
+app.get("/", (req, res) => {
+    res.send("Task Tracker API is running");
+});
 app.listen(3000,()=>{
     console.log(`🚀 Server running  on port 3000`
 )
