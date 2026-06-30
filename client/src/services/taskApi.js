a29bf6ceeb53e8606705b67fc3f26b2c18b8fe70
Zaara-Fatima <bano6096@gmail.com>
2026-06-30 17:39:45 +0530

    Update task API base URL

--- client/src/services/taskApi.js
+++ client/src/services/taskApi.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const taskApi = axios.create({
-    baseURL:"https://task-tracker-phi-six-88.vercel.app/api/tasks",
+    baseURL:"https://task-tracker-chi-bice.vercel.app/api/tasks",
 })
 
 export default taskApi
